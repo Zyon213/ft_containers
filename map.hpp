5e7cb21a72621bcb776b893e6197c9370cc82768
zyon213 <yg.mms2@gmail.com>
2023-06-06 04:32:53 +0400

    map constructor created

--- map.hpp
+++ map.hpp
@@ -5,6 +5,7 @@
 #include <functional>
 #include "utils/pair.hpp"
 #include "utils/lexicographical_compare.hpp"
+#include "utils/reverse_iterator.hpp"
 
 namespace ft
 {
@@ -16,7 +17,7 @@ namespace ft
 	>
 	class map
 	{
-		private:
+		public:
 			typedef Key 											key_type;
 			typedef T 												mapped_type;
 			typedef ft::pair<const key_type, mapped_type>			value_type;	
@@ -33,6 +34,8 @@ namespace ft
 
 			// return a comparision objects that can be used to compare two elements to get
 			// whether the key of the first one goes before the second.
+			
+			/********************** MEMBER CLASS *********************************/
 
 			class value_compare : public std::binary_function<value_type, value_type, bool>
 			{
@@ -48,13 +51,35 @@ namespace ft
 					}
 			};
 
-			explicit map( const key_compare& comp, const Allocator& alloc = Allocator()) {}
+			private:
+				key_compare 		_key_comp;
+				value_compare 		_val_comp;
+				// ft::rbtree<value_type, key_type, value_compare, allocator_type> _tree;
 
-			template<class InputIt>
-			map(InputIt first, InputIt last, const key_compare& comp = Compare(), cosnt Allocator& alloc = Allocator()) {}
-			map(const map& other) {}
+			public:	
+				explicit map( const key_compare& comp = key_compare(), const Allocator& alloc = Allocator()) :
+				_key_comp(comp), _val_comp(comp), _tree(_val_comp, alloc){}
 
+				template<class InputIt>
+				map(InputIt first, InputIt last, const key_compare& comp = key_compare(), cosnt Allocator& alloc = Allocator()) :
+				_key_comp(comp), _val_comp(comp), _tree(_value_comp, alloc)
+				{
+					insert(firts, last);
+				}
+				map(const map& other) : _key_comp(other._key_comp), _val_comp(other._val_comp), _tree(other._tree) {}
 
+				~map() {}
+
+				map& operator=(const map& x)
+				{
+					if (this != &x)
+					{
+						_key_comp = x._key_comp;
+						_val_comp = x._val_comp;
+						_tree = x._tree;
+					}
+					return (*this);
+				}
 	};
 }
 #endif
