0e27616cbadc9dccf1141b279561c83e4cd4388d
zyon213 <yg.mms2@gmail.com>
2023-06-11 03:11:46 +0400

    map progress

--- map.hpp
+++ map.hpp
@@ -18,19 +18,23 @@ namespace ft
 	class map
 	{
 		public:
-			typedef Key 											key_type;
-			typedef T 												mapped_type;
-			typedef ft::pair<const key_type, mapped_type>			value_type;	
-			typedef value_type&										reference;
-			typedef const value_type&								const_reference;
-			typedef Compare											key_compare;
-			typedef Allocator										allocator_type;
-			typedef typename allocator_type::template rebind<value_type>::other type_allocator;
-			typedef std::allocator_traits<allocator_type> 			type_traits;
-			typedef typename type_traits::pointer 					pointer;
-			typedef typename type_traits::const_pointer 			const_pointer;
-			typedef size_t											size_type;
-			typedef	std::ptrdiff_t 									difference_type;
+			typedef Key 																				key_type;
+			typedef T 																					mapped_type;
+			typedef ft::pair<const key_type, mapped_type>												value_type;	
+			typedef value_type&																			reference;
+			typedef const value_type&																	const_reference;
+			typedef Compare																				key_compare;
+			typedef Allocator																			allocator_type;
+			typedef typename allocator_type::template rebind<value_type>::other 						type_allocator;
+			typedef std::allocator_traits<allocator_type> 												type_traits;
+			typedef typename type_traits::pointer 														pointer;
+			typedef typename type_traits::const_pointer 												const_pointer;
+			typedef size_t																				size_type;
+			typedef	std::ptrdiff_t 																		difference_type;
+			typedef typename ft::rbtree<value_type, value_compare, allocator_type>::iterator    		iterator;
+			typedef typename ft::rbtree<value_type, value_compare, allocator_type>::const_iterator    	const_iterator;
+			typedef ft::reverse_iterator<iterator>														reverse_iterator;
+			typedef ft::reverse_iterator<const_iterator>												const_reverse_iterator;
 
 			// return a comparision objects that can be used to compare two elements to get
 			// whether the key of the first one goes before the second.
@@ -51,10 +55,12 @@ namespace ft
 					}
 			};
 
+			/********************** MEMBER FUNCTIONS *********************************/
+
 			private:
-				key_compare 		_key_comp;
-				value_compare 		_val_comp;
-				// ft::rbtree<value_type, key_type, value_compare, allocator_type> _tree;
+				key_compare 														_key_comp;
+				value_compare 														_val_comp;
+				ft::rbtree<value_type, key_type, value_compare, allocator_type> 	_tree;
 
 			public:	
 				explicit map( const key_compare& comp = key_compare(), const Allocator& alloc = Allocator()) :
@@ -80,6 +86,91 @@ namespace ft
 					}
 					return (*this);
 				}
+		
+			/********************** ELEMENT ACCESS *********************************/
+
+			mapped_type& at (const key_type& key)
+			{
+				iterator it = find(key)
+				if (it == end())
+					throw std::out_of_range("map at");
+				return (it->second);
+			}
+			
+			const mapped_type& at (const key_type& key) const
+			{
+				const_iterator it = find(key)
+				if (it == end())
+					throw std::out_of_range("map at");
+				return (it->second);
+			}
+
+			// returns a reference to the value that is mapped to a key or insert
+			// if such key does not alread exist
+
+			mapped_type& operator[](const key_type& key)
+			{
+				ft::pair<iterator, bool> p = insert(ft::make_pair(k, mapped_type()));
+				return (p.first->second);
+			}
+			
+			/********************** ITERATORS *********************************/
+
+			iterator begin() { return (_tree.begin()); }
+
+			const_iterator begin() { return (_tree.begin()); }
+
+			iterator end() { return (_tree.end()); }
+
+			const_iterator end() { return (_tree.end()); }
+
+			reverse_iterator rbegin() { return (reverse_iterator.end()); }
+
+			const_reverse_iterator begin() { return (reverse_iterator.end()); }
+
+			reverse_iterator rend() { return (reverse_iterator.begin()); }
+
+			const_reverse_iterator rend() { return (reverse_iterator.begin()); }
+
+			/********************** CAPACITY *********************************/
+
+			bool empty () const { return _tree.empty(); }
+
+			size_type size () const { return (_tree.size()); }
+			
+			size_type max_size () const { return (_tree.max_size()); }
+
+			/********************** MODIFIERS *********************************/
+
+			void clear () { _tree.clear();}
+
+			ft::pair<iterator, bool> insert(const value_type& value) {
+				return (_tree.insert(value));
+			}
+
+			iterator insert(iterator pos, const value_type& value) {
+				return (_tree.insert(pos, value));
+			}
+
+			template<class InputIt>
+			void insert(InputIt first, InputIt last) {
+				return (_tree.insert(first, last));
+			}
+
+			void erase(iterator position) { _tree.erase(position);}
+
+			size_type erase (const key_type& k) { return (_tree.erase(k));}
+
+			void erase (iterator first, iterator last) { _tree.erase(first, last); }
+
+			void swap (map& x) { _tree.swap(x._tree);}
+
+			/********************** OPERATIONS *********************************/
+			
+
+
+
+
 	};
 }
 #endif
