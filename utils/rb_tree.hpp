3caeff0c95119b38fc22f1ee182cba32e053955d
zyon213 <yg.mms2@gmail.com>
2023-06-12 05:53:25 +0400

    rb-tree iterator

--- utils/rb_tree.hpp
+++ utils/rb_tree.hpp
@@ -123,5 +123,138 @@ namespace ft
 		return (!comp(u, v) && (!comp(v, u)));
 	}
 
+	template <typename U, typename U>
+	class tree_iterator
+	{
+		public:
+			typedef	U																value_type;
+			typedef value_type*														pointer;
+			typedef value_type&														reference;
+			typedef V*																iterator_type;
+			typedef typename iterator_traits<iterator_type>::difference_type 		difference_type;
+			typedef typename iterator_traits<iterator_type>::value_type 			node_type;
+			typedef typename iterator_traits<iterator_type>::pointer 				node_pointer;
+			typedef typename iterator_traits<iterator_type>::reference 				node_reference;
+			typedef typename iterator_traits<iterator_type>::iterator_category 		iterator_category;
+		
+		private:
+			node_pointer 		_current;
+			node_pointer		_nil;
+		
+		public:
+			tree_iterator() : _current(ft::NIL), _nil(ft::NIL) {}
+			tree_iterator(node_pointer cur, node_pointer nil) : _current(cur), _nil(nil) {}
+			tree_iterator(const tree_iterator& node) : _current(node._current), _nil(node._nil) {}
+			~tree_iterator() {}
+
+			tree_iterator& operator=(const tree_iterator& node)
+			{
+				if (this != node)
+				{
+					_current = node._current;
+					_nil = node._nil;
+				}
+				return (*this);
+			}
+
+			node_pointer base() const
+			{
+				return (_current);
+			}
+
+			pointer operator->() const 
+			{
+				return (&_current->_value);
+			}
+
+			reference operator*() const
+			{
+				return (_current->_value);
+			}
+
+			tree_iterator operator++()
+			{
+				_current = get_next_node(_current, _nil)
+				return (*this);
+			}
+
+			tree_iterator operator++(int)
+			{
+				tree_iterator tmp(*this);
+				++(*this);
+				return (tmp);
+			}
+
+			tree_iterator operator--()
+			{
+				_current = get_prev_node(_current, _nil)
+				return (*this);
+			}
+
+			tree_iterator operator++(int)
+			{
+				tree_iterator tmp(*this);
+				--(*this);
+				return (tmp);
+			}
+
+			template <typename T>
+			bool operator==(const tree_iterator<T, node_type>& x) const
+			{
+				return (_current == x.base());
+			}
+
+			template <typename T>
+			bool operator!=(const tree_iterator<T, node_type>& x) const
+			{
+				return !(*this == x);
+			}
+
+			operator tree_iterator<const value_type, node_type>(void) const
+			{
+				return (tree_iterator<const value_type, node_type>(_current, _nil));
+			}
+
+			friend bool operator==(const tree_iterator& lhs, const tree_iterator& rhs)
+			{
+				return (lhs._current == rhs._current);
+			}
+
+			friend bool operator!=(const tree_iterator& lhs, const tree_iterator& rhs)
+			{
+				return (!(lhs == rhs));
+			}
+	};
+
+		template <typename Alloc>
+		struct allocator_traits
+		{
+			typedef typename Alloc::value_type 				value_type;
+			typedef typename Alloc::pointer					pointer;
+			typedef typename Alloc::const_pointer			const_pointer;
+
+			static Alloc allocator(Alloc& a, std::size_t n)
+			{
+				return a.allocate(n);
+			}
+
+			static void deallocate(Alloc& a, value_type* p, std::size_t n)
+			{
+				a.deallocate(p, n);
+			}
+
+			template <typename T>
+			static void construct(Alloc& a, T* p, const T& val)
+			{
+				a.construct(p, val);
+			}
+
+			template <typename T>
+			static void destroy(Alloc& a, T* p)
+			{
+				a.destroy(p);
+			}
+
+		};
 }
 #endif
